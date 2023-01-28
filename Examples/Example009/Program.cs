5363b846063c5c530d26bf8e31ef8515c58dc69a
Novikova Ksusha <sushi.91@mail.ru>
2023-01-28 19:52:17 +0700

    давбавлено 2 примера

---
+++ Examples/Example009/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+int [] array = {14,21,3,45,5,6,87,8,99};
+array [0]=12;
+Console.WriteLine(array[8]);
+
+
+
---
+++ Examples/example010/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 18, 26, 33, 4, 95, 76, 57, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+while (index<n)
+
+{
+
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
