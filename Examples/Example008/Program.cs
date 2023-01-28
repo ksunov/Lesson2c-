bc8b8b4d7ed8f79e555d14f33106448acde15d8e
Novikova Ksusha <sushi.91@mail.ru>
2023-01-28 19:15:46 +0700

    обновила функцию

--- Examples/Example008/Program.cs
+++ Examples/Example008/Program.cs
@@ -6,7 +6,7 @@
     return result;
 }
 
-int a1 = 15;
+int a1 = 11615;
 int b1 = 21;
 int c1 = 39;
 int a2 = 15282;
@@ -16,10 +16,10 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 33;
 
-int max1 = Max(a1,b1,c1);
-int max2 = Max(a2,b2,c2);
-int max3 = Max(a3,b3,c3);
-int max = Max(max1,max2,max3)
+int max = Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
 
 Console.WriteLine(max);
 
